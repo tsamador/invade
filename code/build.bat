0d96ce62bfa0b24c7954a0e50283e3ae29b0e5f6
Tanner Amador <tamador97@gmail.com>
2021-12-18 15:11:38 -0700

    Intial Commit

---
+++ code/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+mkdir ..\build
+pushd ..\build
+cl -Zi /w /EHsc /MTd ..\code\gl_invade.cpp ..\code\ShaderLoader.cpp ..\code\glad.c ..\code\stb_image.cpp /link  /LIBPATH:..\lib OpenGL32.lib glfw3.lib user32.lib gdi32.lib Shell32.lib
+popd
